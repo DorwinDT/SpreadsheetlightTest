59a992c45203d9051d3d1ac6c94b7bd724470279
Peter <peter.turcan@tekl.com>
2022-07-14 16:32:13 +0200

    Add project files.

---
+++ SpeedLightTestCorrect/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpreadsheetLight;
+
+namespace SpeedLightTestCorrect
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("SpreadSheetLight test app - Correct version");
+			using (var sl = new SLDocument(@"reportTemplate1.xlsx"))
+			{
+				sl.Save();
+			}
+		}
+	}
+}
---
+++ SpeedLightTestError/Program.cs
@@ -0,0 +1,21 @@
+﻿using SpreadsheetLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetlightTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("SpreadSheetLight test app - Error version");
+			using (var sl = new SLDocument(@"reportTemplate1.xlsx"))
+			{
+				sl.Save();
+			}
+		}
+	}
+}
